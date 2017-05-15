4decb963d8fc4f453cb451357a6826a2912e887a
Fewfy <1215178962@qq.com>
2017-05-15 11:02:56 +0800

    readme

--- Regeneration/Regeneration/main.cpp
+++ Regeneration/Regeneration/main.cpp
@@ -3,8 +3,24 @@
 
 using namespace cv;
 
+Mat composit(Mat srcA, Mat srcB, Mat srcC) {
+	Mat res = srcB;
+	for (int i = 0; i < srcB.rows; i++) {
+		for (int j = 0; j < srcC.cols; j++) {
+			if (srcC.at<int>(i,j) != 0 && srcB.at<int>(i,j) == 0) {
+				res.at<int>(i,j) = srcC.at<int>(i,j);
+			}
+		}
+	}
+	return res;
+}
+
 int main() {
-	Mat src = imread("data/B.png");
-	imshow("blackwhite", src);
+	Mat srcA = imread("data/A.png");
+	Mat srcB = imread("data/B.png");
+	Mat srcC = imread("data/C.png");
+	imshow("A", srcA);
+	imshow("B", srcB);
+	imshow("C", srcC);
 	waitKey(0);
 }
